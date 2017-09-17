2e03c73407f568bf92205f6ccc5458e7e8323c83
lxs623 <lxs623@student.bham.ac.uk>
2017-09-17 21:26:34 +0100

    Initial cbc implementation

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,6 @@ pub fn ecb_encrypt(key: &[u8], data: &[u8]) -> Vec<u8> {
     output
 }
 
-
 pub fn ecb_decrypt(key: &[u8], data: &[u8]) -> Vec<u8> {
     let ekey = aes::expand_key(key);
     let mut output = Vec::new();
@@ -26,4 +25,41 @@ pub fn ecb_decrypt(key: &[u8], data: &[u8]) -> Vec<u8> {
     }
     output
 }
+
+pub fn cbc_encrypt(key: &[u8], data: &[u8]) -> Vec<u8> {
+    let ekey = aes::expand_key(key);
+    let mut output = Vec::new();
+    let mut previous = [0; 16];
+    for block in data.chunks(16) {
+        if block.len() != 16 {
+            panic!("Size must be multiple of 16 bytes");
+        }
+        for i in 0..16 {
+            previous[i] ^= block[i];
+        }
+        let encrypted = aes::encrypt(&ekey, &previous);
+        previous = encrypted;
+        output.extend(encrypted.iter().cloned());
+    }
+    output
+}
+
+pub fn cbc_decrypt(key: &[u8], data: &[u8]) -> Vec<u8> {
+    let ekey = aes::expand_key(key);
+    let mut output = Vec::new();
+    let mut previous = [0; 16];
+    for block in data.chunks(16) {
+        if block.len() != 16 {
+            panic!("Size must be multiple of 16 bytes");
+        }
+        let mut decrypted = aes::decrypt(&ekey, block);
+        for i in 0..16 {
+            decrypted[i] ^= previous[i];
+            previous[i] = block[i];
+        }
+        output.extend(decrypted.iter().cloned());
+    }
+    output
+}
+
 
