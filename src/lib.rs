e74b9fa81070d0c651d8102021a547696db3aa3e
lxs623 <lxs623@student.bham.ac.uk>
2017-09-17 21:41:11 +0100

    Added cbc iv specification

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,7 @@ pub fn ecb_encrypt(key: &[u8], data: &[u8]) -> Vec<u8> {
     let ekey = aes::expand_key(key);
     let mut output = Vec::new();
     for block in data.chunks(16) {
-        if block.len() != 16 {
-            panic!("Size must be multiple of 16 bytes");
-        }
+        if block.len() != 16 { panic!("Size must be multiple of 16 bytes"); }
         let encrypted = aes::encrypt(&ekey, block);
         output.extend(encrypted.iter().cloned());
     }
@@ -17,23 +15,20 @@ pub fn ecb_decrypt(key: &[u8], data: &[u8]) -> Vec<u8> {
     let ekey = aes::expand_key(key);
     let mut output = Vec::new();
     for block in data.chunks(16) {
-        if block.len() != 16 {
-            panic!("Size must be multiple of 16 bytes");
-        }
+        if block.len() != 16 { panic!("Size must be multiple of 16 bytes"); }
         let decrypted = aes::decrypt(&ekey, block);
         output.extend(decrypted.iter().cloned());
     }
     output
 }
 
-pub fn cbc_encrypt(key: &[u8], data: &[u8]) -> Vec<u8> {
+pub fn cbc_encrypt(key: &[u8], data: &[u8], iv: &[u8]) -> Vec<u8> {
     let ekey = aes::expand_key(key);
     let mut output = Vec::new();
     let mut previous = [0; 16];
+    for i in 0..16 { previous[i] = iv[i]; }
     for block in data.chunks(16) {
-        if block.len() != 16 {
-            panic!("Size must be multiple of 16 bytes");
-        }
+        if block.len() != 16 { panic!("Size must be multiple of 16 bytes"); }
         for i in 0..16 {
             previous[i] ^= block[i];
         }
@@ -44,14 +39,13 @@ pub fn cbc_encrypt(key: &[u8], data: &[u8]) -> Vec<u8> {
     output
 }
 
-pub fn cbc_decrypt(key: &[u8], data: &[u8]) -> Vec<u8> {
+pub fn cbc_decrypt(key: &[u8], data: &[u8], iv: &[u8]) -> Vec<u8> {
     let ekey = aes::expand_key(key);
     let mut output = Vec::new();
     let mut previous = [0; 16];
+    for i in 0..16 { previous[i] = iv[i]; }
     for block in data.chunks(16) {
-        if block.len() != 16 {
-            panic!("Size must be multiple of 16 bytes");
-        }
+        if block.len() != 16 { panic!("Size must be multiple of 16 bytes"); }
         let mut decrypted = aes::decrypt(&ekey, block);
         for i in 0..16 {
             decrypted[i] ^= previous[i];
